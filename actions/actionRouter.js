9a575f442954954ad092fe316b5832f27a9367ab
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-06-12 21:54:12 -0400

    CRUD COMPLETED.

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -3,9 +3,8 @@ const db = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
-router.get("/:id", (req, res, next) => {
-  console.log(req.params.id);
-  db.get(req.params.id)
+router.get("/", (req, res, next) => {
+  db.get(req.body.id)
     .then((user) => {
       console.log(user);
       res.status(200).json(user);
@@ -33,7 +32,7 @@ router.post("/", (req, res, next) => {
 });
 
 router.delete("/:id", (req, res, next) => {
-  db.remove(req.param.id)
+  db.remove(req.params.id)
     .then((count) => {
       if (count > 0) {
         res.status(200).json({ message: "Bye Project" });
@@ -44,6 +43,24 @@ router.delete("/:id", (req, res, next) => {
     .catch(next);
 });
 
-// router.put
+router.put("/:id", (req, res, next) => {
+  if ((!req.body.project_id && !req.body.description) || !req.body.notes) {
+    return res.status(400).json({ message: "You need all your info" });
+  }
+  const putOBJ = {
+    project_id: req.body.project_id,
+    description: req.body.description,
+    notes: req.body.notes,
+  };
+  db.update(req.params.id, putOBJ)
+    .then((project) => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: "no id" });
+      }
+    })
+    .catch(next);
+});
 
 module.exports = router;
