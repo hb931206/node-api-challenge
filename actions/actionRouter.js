5134e9946342d2a7249ff695fba0a43d3d66a7f3
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-06-12 21:42:58 -0400

    Post Call with James. GET ACTIONS and GET POST are working.

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -3,9 +3,11 @@ const db = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  db.get(req.body.id)
+router.get("/:id", (req, res, next) => {
+  console.log(req.params.id);
+  db.get(req.params.id)
     .then((user) => {
+      console.log(user);
       res.status(200).json(user);
     })
     .catch(next);
@@ -26,6 +28,18 @@ router.post("/", (req, res, next) => {
   db.insert(postOBJ)
     .then((project) => {
       res.status(201).json(project);
+    })
+    .catch(next);
+});
+
+router.delete("/:id", (req, res, next) => {
+  db.remove(req.param.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: "Bye Project" });
+      } else {
+        res.status(404).json({ message: "Couldn't find ID" });
+      }
     })
     .catch(next);
 });
